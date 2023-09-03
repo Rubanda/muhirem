c8ac4bcb7581a7c9e547caba9026c397a92908fb
Moise Muhire <rubanda57@gmail.com>
2023-09-03 20:26:25 +0300

    added content on the about and projects

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Projects() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h3 className='font-bold text-xl'>Projects</h3>
+    </main>
+  )
+}
