526be4d6d5de32768d94974c7e5da67d7488e18e
Moise Muhire <rubanda57@gmail.com>
2023-10-29 20:57:21 +0200

    commit testing osborn

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="m-auto max-w-2xl flex flex-col px-3">
-      <h3 className='font-extrabold tracking-tight mb-6'>hey, i&apos;m Moise👋🏽</h3>
+      <h3 className='font-extrabold tracking-tight mb-6'>Hey, i&apos;m Moise👋🏽</h3>
       <p className='text-base text-muted-foreground'>
         I&apos;m a backend engineer at Gunsel, passionate about web development
         and dedicated to creating robust and efficient solutions for the digital world.
       </p>
+      <div className='border-2 border-solid border-black p-23'>
+      <Button className='rounded-full bg-pink-600 text-white animate-bounce' >Osborn</Button>
 
+      </div>
     </main>
   )
 }
